3aaf130c3aa19a3e5f3ea89df8de193ae2523946
Dmitriy Golovko <dmitriygolovko00@gmail.com>
2025-04-07 15:47:38 +0000

    Added permissions

--- countips.sh
+++ countips.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
 #Count number of login attempts from different hosts
-#then ask to ban them
-#
-#The banning process uses the access control files for tcp wrappers
-#in order to ban ip addresses flat out.
-
-
 
 TMP_FILE="/tmp/ip/ipss.txt"
 COUNT_FILE="/tmp/ip/ipsc.txt"
 
-#If attempts are greater than this the
-#IP will be banned.
-MAX_ATTEMPTS=0
-ALLOWED_ATTEMPTS=5 #Allowed amount of login attempts from IP
 SINCE_DATE=""
 
 echo -e "Date since (type nothing for entire history) (Use format YYYY-MM-DD)"
@@ -39,48 +29,3 @@ cat $COUNT_FILE
 
 echo -e "\nTotal failed login attempts: \c"
 echo `wc -l $TMP_FILE | grep -oE "[0-9]+"`
-
-#Ban the IP address using access control see man hosts_access(5)
-#One argument: IP address to ban
-function ban_ip() {
-        if [ "$(cat /etc/hosts.deny | grep "$1")" = "" ]; then
-                if [ -f "/etc/hosts.deny" ]; then
-                        echo ALL : $1 : deny >> /etc/hosts.deny
-                        echo -e "Banning IP \c"
-                        echo $1
-                else
-                        echo "/etc/hosts.deny does not exist"
-                        exit 1
-                fi
-        fi
-}
-
-
-echo ""
-
-
-if [ ! [ -w /etc/hosts.deny -o -r /etc/hosts.deny ] ]; then
-        echo -e "***Don't have permissions for access control***"
-        echo "Set /etc/hosts.deny READ & WRITE permissions"
-        exit 1
-fi
-
-
-while (( MAX_ATTEMPTS < ALLOWED_ATTEMPTS + 1 )); do
-        echo -e "Set max attempts and ban IPs ( >$ALLOWED_ATTEMPTS ): \c"
-        read MAX_ATTEMPTS
-done
-
-
-cat $COUNT_FILE | while IFS= read -r line; do
-        ATTEMPTS=$(echo "$line" | awk '{print $1}')
-        if [ $ATTEMPTS -gt $MAX_ATTEMPTS ]; then
-                echo -e "Banning IP \c"
-                IP="$(echo $line | awk '{print $2}')"
-                echo $IP
-                ban_ip $IP
-        fi
-done
-
-#echo -e "\n*****READING FILE /etc/hosts.deny*****"
-#cat /etc/hosts.deny
